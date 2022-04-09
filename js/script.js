cf66dfdc2a41fd8ba930c26c0caf0b1add1dc911
yesferbany <yesfer.bany@gmail.com>
2022-04-08 23:00:49 -0600

    responsive web

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
-
 document.getElementById("back_menu").addEventListener("click", ocultar_menu);
 
 nav = document.getElementById("nav");
 background_menu = document.getElementById("back_menu");
 
+/* mostrando menu */
 function mostrar_menu(){
 
     nav.style.right = "0px";
     background_menu.style.display = "block";
 }
 
+/* ocultando menu */
 function ocultar_menu(){
 
     nav.style.right = "-250px";
