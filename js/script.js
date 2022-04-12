9cdae9be98941882c366a4be750c3c1bd213040f
yesferbany <yesfer.bany@gmail.com>
2022-04-12 00:13:38 -0600

    mejoras en button up

--- js/script.js
+++ js/script.js
@@ -29,26 +29,16 @@ menuLinks.forEach(menuLink => {
 })
 
 /* BOTON HACIA ARRIBA */
-document.getElementById("button-up").addEventListener("click", scrollUp);
+const currentScroll = document.getElementById("button-up")
 
-function scrollUp(){
+currentScroll.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
 
-    var currentScroll = document.documentElement.scrollTop;
-
-    if(currentScroll > 0){
-        window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 1.5));
-    }
-}
-
-buttonUp = document.getElementById("button-up");
-
-window.onscroll = function(){
-    var scroll = document.documentElement.scrollTop;
-
-    if(scroll > 1900){
-        buttonUp.style.transform = "scale(1)";
-    }else if(scroll < 1900){
-        buttonUp.style.transform = "scale(0)";
+window.onscroll = () => {
+    if(window.scrollY < 1500){
+        currentScroll.classList.remove("btn-scrolltop-on")
+    }else{
+        currentScroll.classList.add("btn-scrolltop-on")
     }
 }
